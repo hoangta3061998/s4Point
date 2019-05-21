2cac419d28a171eeebca068b10e7badeb2390d63
HoangNguyen <h01694111033@gmail.com>
2019-05-21 12:00:02 +0700

    a

---
+++ src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        Point3D p1 = new Point3D();
+        Point3D p2 = new Point3D(5,6,7);
+        System.out.println(p1);
+        System.out.println(p2);
+    }
+}
